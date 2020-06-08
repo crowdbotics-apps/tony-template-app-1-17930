e782aab257dda00dacbf2f2ba780c19eb58fa39d
Crowdbotics <team@crowdbotics.com>
2020-06-08 06:47:32 +0000

    Update models

--- backend/tony_template_app_1_17930/wsgi.py
+++ backend/tony_template_app_1_17930/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tony_template_app_1_17930.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tony_template_app_1_17930.settings")
 
 application = get_wsgi_application()
